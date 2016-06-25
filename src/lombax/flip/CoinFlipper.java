7c0ccff1bd63755de2971af5a1423bc9b3951ffd
lombax5832 <iPodAlexei@gmail.com>
2016-06-24 20:46:42 -0400

    Separate Class to hold constants

--- src/lombax/flip/CoinFlipper.java
+++ src/lombax/flip/CoinFlipper.java
@@ -1,9 +1,6 @@
 package lombax.flip;
 
 public class CoinFlipper{
-
-	public static final int ATTEMPTS = 1000000;
-	public static final int SAME_IN_A_ROW = 9;
 	
 	public static void main(String[] args) {
 		int successful = 0;
@@ -16,12 +13,12 @@ public class CoinFlipper{
 		
 		FlipCoin coin = new FlipCoin();
 		startTime=System.currentTimeMillis();
-		while(successful<ATTEMPTS){
+		while(successful<Constants.ATTEMPTS){
 			
 			counter = 0;
 			
 			// Runs as long as we have less than the amount we're looking for
-			while(counter<SAME_IN_A_ROW){
+			while(counter<Constants.SAME_IN_A_ROW){
 				if(coin.flip()){
 					counter++;
 					heads++;
@@ -32,18 +29,19 @@ public class CoinFlipper{
 				total++;
 			}
 			
-			if(counter==SAME_IN_A_ROW){
+			if(counter==Constants.SAME_IN_A_ROW){
 				successful++;
 				if((successful%10000)==0){
-					System.out.println(ATTEMPTS-successful+" successful tries remaining.");
+					System.out.println(Constants.ATTEMPTS-successful+" successful tries remaining.");
 				}
 			}
 		}
 		
 		endTime=System.currentTimeMillis();
+		//Print out information to the user
 		System.out.println(1.0*(endTime-startTime)/1000.0 + " seconds to process " + total + " coin flips.");
 		System.out.println("Heads:Tails ("+heads+':'+tails+") ratio = " + 1.0*heads/tails);
-		System.out.println(1.0*total/ATTEMPTS);
+		System.out.println(1.0*total/Constants.ATTEMPTS);
 	}
 
 }
---
+++ src/lombax/flip/Constants.java
@@ -0,0 +1,8 @@
+package lombax.flip;
+
+public class Constants {
+	//Amount of successful attempts we want before termination
+	public static final int ATTEMPTS = 10000000;
+	//Amount of heads we want in a row to count as an attempt
+	public static final int SAME_IN_A_ROW = 5;
+}
